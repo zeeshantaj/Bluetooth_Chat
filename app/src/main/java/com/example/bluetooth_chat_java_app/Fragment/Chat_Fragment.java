0a829d592cf055064ef70daf2e260cb05cdb3387
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-01-08 22:59:33 +0500

    first8 commit

---
+++ app/src/main/java/com/example/bluetooth_chat_java_app/Fragment/Chat_Fragment.java
@@ -0,0 +1,30 @@
+package com.example.bluetooth_chat_java_app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.bluetooth_chat_java_app.R;
+
+public class Chat_Fragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.chat_layout,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+    }
+}
