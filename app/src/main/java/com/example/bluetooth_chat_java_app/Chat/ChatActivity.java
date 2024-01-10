922c79efdd3f4d69f089c2aff15ab73b79cd94c0
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-01-10 15:57:50 +0500

    second9 commit

--- app/src/main/java/com/example/bluetooth_chat_java_app/Chat/ChatActivity.java
+++ app/src/main/java/com/example/bluetooth_chat_java_app/Chat/ChatActivity.java
@@ -3,14 +3,56 @@ package com.example.bluetooth_chat_java_app.Chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.bluetooth_chat_java_app.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ChatActivity extends AppCompatActivity {
+    private EditText messageEd;
+    private FloatingActionButton sendBtn;
+    private ImageView cameraImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        messageEd = findViewById(R.id.chatEd);
+        sendBtn = findViewById(R.id.sendBtn);
+        cameraImg = findViewById(R.id.cameraImg);
+
+        setIconVisibility();
+
+        messageEd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                setIconVisibility();
+            }
+        });
+    }
+    private void setIconVisibility(){
+        if (!messageEd.getText().toString().isEmpty()){
+            sendBtn.setImageResource(R.drawable.send_icon);
+            cameraImg.setVisibility(View.GONE);
+        }
+        else {
+            sendBtn.setImageResource(R.drawable.mic_icon);
+            cameraImg.setVisibility(View.VISIBLE);
+        }
+
     }
 }
