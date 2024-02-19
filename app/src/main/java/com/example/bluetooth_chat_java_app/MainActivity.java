a84be6e586878ec48092bbb9bea9fe1f6d9fe5c6
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-02-19 22:28:29 +0500

    scan feature commit

--- app/src/main/java/com/example/bluetooth_chat_java_app/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_chat_java_app/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.bluetooth_chat_java_app;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
@@ -16,9 +19,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -26,14 +31,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.bluetooth_chat_java_app.Controller.ChatController;
-import com.example.bluetooth_chat_java_app.Fragment.Chat_Fragment;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -41,25 +45,20 @@ public class MainActivity extends AppCompatActivity {
     private BluetoothAdapter mAdapter;
     private BluetoothSocket mSocket;
     private static final int REQUEST_ENABLE_CODE = 1;
-    private static final int REQUEST_BLUETOOTH_PERMISSION = 2;
-    private ArrayAdapter<String> mDiscoveredDevicesArrayAdapter;
     private ArrayAdapter<String> mArrayAdapter;
-    private Button scanBtn;
-    private ChatController chatController;
-    public static final int MESSAGE_STATE_CHANGE = 1;
-    public static final int MESSAGE_READ = 2;
-    public static final int MESSAGE_WRITE = 3;
-    public static final int MESSAGE_DEVICE_OBJECT = 4;
-    public static final int MESSAGE_TOAST = 5;
     public static final String DEVICE_OBJECT = "device_name";
     private ProgressBar progressBar;
+    private Button btScanBtn;
+    private List<BluetoothDevice> deviceList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         progressBar = findViewById(R.id.progressBar);
+        btScanBtn = findViewById(R.id.btScanBTn);
 
-        chatController = new ChatController(this,new Handler());
+        deviceList = new ArrayList<>();
+
         mAdapter = BluetoothAdapter.getDefaultAdapter();
 
         if (mAdapter == null) {
@@ -67,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Bluetooth is not supported on this device", Toast.LENGTH_SHORT).show();
             finish();
         } else {
+            checkAndRequestPermissions();
             if (!mAdapter.isEnabled()) {
                 // Bluetooth is not enabled, request to turn it on
                 Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -77,7 +77,80 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        // Register broadcast receiver and start discovery
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(receiver, filter);
+        mAdapter.startDiscovery();
+
+
+//        ActivityResultLauncher<String[]> locationPermissionRequest =
+//                registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),
+//                        result -> {
+//                            Boolean fineLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false);
+//                            Boolean coarseLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false);
+//                            if (fineLocationGranted || coarseLocationGranted) {
+//                                // Location access granted.
+//                                // Proceed with Bluetooth scanning or other location-dependent tasks.
+//                                Toast.makeText(this, "Location permission granted", Toast.LENGTH_SHORT).show();
+//                            } else {
+//                                // Location access not granted.
+//                                Toast.makeText(this, "Location permission not granted", Toast.LENGTH_SHORT).show();
+//                                // Check if permission is denied permanently
+//                                if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION) &&
+//                                        !ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+//                                    // Permission denied permanently, prompt user to enable in settings
+//                                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//                                    builder.setTitle("Permission Required");
+//                                    builder.setMessage("Location permission is required for Bluetooth scanning. Please enable it in the app settings.");
+//                                    builder.setPositiveButton("Go to Settings", (dialog, which) -> {
+//                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+//                                        Uri uri = Uri.fromParts("package", getPackageName(), null);
+//                                        intent.setData(uri);
+//                                        startActivity(intent);
+//                                    });
+//                                    builder.setNegativeButton("Cancel", null);
+//                                    builder.show();
+//                                }
+//                            }
+//                        }
+//                );
+        btScanBtn.setOnClickListener(v -> {
+            // Check and request permissions before starting a new scan
+            discoverDevice();
+            Toast.makeText(this, "clk", Toast.LENGTH_SHORT).show();
+        });
+//
+
     }
+
+    private void discoverDevice() {
+        mAdapter.startDiscovery();
+    }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                Log.e("MyApp","device available"+deviceName);
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver,intentFilter);
+    }
+
     private void showPairedDevices() {
         Set<BluetoothDevice> pairedDevices = mAdapter.getBondedDevices();
         ArrayList<String> devicesList = new ArrayList<>();
@@ -111,7 +184,52 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+
+
+    private void checkAndRequestPermissions() {
+        List<String> permissionsNeeded = new ArrayList<>();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
+            permissionsNeeded.add(Manifest.permission.BLUETOOTH);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED) {
+            permissionsNeeded.add(Manifest.permission.BLUETOOTH_ADMIN);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionsNeeded.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        if (!permissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsNeeded.toArray(new String[0]), REQUEST_ENABLE_CODE);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_CODE) {
+            if (resultCode == RESULT_OK) {
+                // User enabled Bluetooth
+                Toast.makeText(this, "Bluetooth is Enabled", Toast.LENGTH_SHORT).show();
+                showPairedDevices();
+            } else if (resultCode == RESULT_CANCELED) {
+                // User didn't enable Bluetooth
+                Toast.makeText(this, "Bluetooth is not Enabled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+    private void updateUI() {
+        ArrayList<String> deviceNameList = new ArrayList<>();
+        for (BluetoothDevice device : deviceList) {
+            deviceNameList.add(device.getName() + "\n" + device.getAddress());
+        }
+        mArrayAdapter.clear();
+        mArrayAdapter.addAll(deviceNameList);
+        mArrayAdapter.notifyDataSetChanged();
     }
     private void connectToDevice(String deviceAddress) {
 
@@ -169,58 +287,13 @@ public class MainActivity extends AppCompatActivity {
             if (isConnected) {
                 Toast.makeText(MainActivity.this, "Connected to device", Toast.LENGTH_SHORT).show();
                 // You're now connected and can perform further actions if needed
-                displayChatFragment();
 
             } else {
                 Toast.makeText(MainActivity.this, "Failed to connect to device", Toast.LENGTH_SHORT).show();
-                disconnectFromDevice();
             }
         }
 
     }
-    private void displayChatFragment() {
-        Chat_Fragment chatFragment = new Chat_Fragment();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragmentContainer, chatFragment); // R.id.fragmentContainer is the container in your layout to replace with the fragment
-        fragmentTransaction.addToBackStack(null); // Optional: Add transaction to back stack for back navigation
-        fragmentTransaction.commit();
-    }
-    private void disconnectFromDevice() {
-        // Perform disconnection logic here
 
-        if (mSocket != null) {
-            try {
-                mSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            mSocket = null;
-        }
-
-        // Check if ChatFragment is currently visible
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment chatFragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
-
-        if (chatFragment instanceof Chat_Fragment) {
-            // ChatFragment is visible, remove it
-            fragmentManager.beginTransaction().remove(chatFragment).commit();
-        }
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_ENABLE_CODE) {
-            if (resultCode == RESULT_OK) {
-                // User enabled Bluetooth
-                Toast.makeText(this, "Bluetooth is Enabled", Toast.LENGTH_SHORT).show();
-                showPairedDevices();
-            } else if (resultCode == RESULT_CANCELED) {
-                // User didn't enable Bluetooth
-                Toast.makeText(this, "Bluetooth is not Enabled", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        }
-    }
 
 }
